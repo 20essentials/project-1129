9a8fca9364f24a2e1f56466d5f4d7d63d56d835f
DominicDev <s0l0estudi4r@gmail.com>
2025-08-14 14:34:34 +0200

    v0.0.8

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   // base: './',
 
   //Deploy only with Github Pages, with Vercel is not neccesary 
-  base: '/project-1129/',
+  // base: '/project-1129/',
 
   plugins: [react()],
   resolve: {
