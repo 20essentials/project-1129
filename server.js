dffc00115e0c77037a77dbe8cee19a2f182e951c
DominicDev <s0l0estudi4r@gmail.com>
2025-08-14 14:20:19 +0200

    v0.0.4

--- server.js
+++ server.js
@@ -3,15 +3,12 @@ import { join } from "path";
 
 const app = express();
 
-// Servir la carpeta dist
 app.use(express.static(join(import.meta.dirname, "dist")));
 
-// Manejar rutas de SPA
 app.get("*", (req, res) => {
   res.sendFile(join(import.meta.dirname, "dist", "index.html"));
 });
 
-// Puerto que asigna Railway
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`✅ Servidor en puerto ${PORT}`);
